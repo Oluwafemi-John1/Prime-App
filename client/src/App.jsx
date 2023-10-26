d3800f44bc2b30df6ac7f8c435202c6bef5629be
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-10-26 16:38:07 +0100

    Google Sign Up added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,26 +3,9 @@ import "./App.css";
 import Home from "./components/Home";
 import CreateAccount from "./components/CreateAccount";
 import SignIn from "./components/SignIn";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-
 
 
 function App() {
-    const firebaseConfig = {
-        apiKey: "AIzaSyCHBKiHENOFMnPZHc_g64a7PztfLQaH5Uc",
-        authDomain: "entertainment-app-prime.firebaseapp.com",
-        projectId: "entertainment-app-prime",
-        storageBucket: "entertainment-app-prime.appspot.com",
-        messagingSenderId: "186663723752",
-        appId: "1:186663723752:web:ff8a5d00bf95dfc3351f5d",
-        measurementId: "G-98XJJN2X4Q"
-    }
-
-    const app = initializeApp(firebaseConfig)
-    const analytics = getAnalytics(firebaseConfig)
-    const auth = getAuth(app)
     return (
         <>
             <Routes>
--- client/src/components/CreateAccount.jsx
+++ client/src/components/CreateAccount.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import pic from '../components/assets/images/Mobile login.gif';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const CreateAccount = () => {
     let mainDiv = {
@@ -10,6 +12,31 @@ const CreateAccount = () => {
 
     let signUp = {
         padding: "5px 50px"
+    }
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyCHBKiHENOFMnPZHc_g64a7PztfLQaH5Uc",
+        authDomain: "entertainment-app-prime.firebaseapp.com",
+        projectId: "entertainment-app-prime",
+        storageBucket: "entertainment-app-prime.appspot.com",
+        messagingSenderId: "186663723752",
+        appId: "1:186663723752:web:ff8a5d00bf95dfc3351f5d",
+        measurementId: "G-98XJJN2X4Q"
+    }
+
+    const app = initializeApp(firebaseConfig)
+    const auth = getAuth(app)
+    const provider = new GoogleAuthProvider()
+
+    const signInG = () => {
+        // alert("Hello")
+        signInWithPopup(auth, provider)
+        .then((result)=>{
+            console.log(result);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
     }
 
   return (
@@ -56,7 +83,7 @@ const CreateAccount = () => {
                                 <span>Or</span>
                                 <hr className="line"/>
                                 </div>
-                                <button title="Sign In" type="submit" className="sign-in_ggl my-2">
+                                <button title="Sign In" type="submit" className="sign-in_ggl my-2" onClick={signInG}>
                                     <svg height="18" width="18" viewBox="0 0 32 32" xmlnsXlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
                                         <defs>
                                             <path d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z" id="A"></path>
