fa555f4fea189cb450960cfa4c473a21fbf6ae86
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-10-26 22:32:39 +0100

    mongoose setup

--- server/index.js
+++ server/index.js
@@ -3,15 +3,24 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
 
 
 // Variable Declarations
 const port = process.env.port
+const mongodb_uri = process.env.URI
 
 // Middlewares
 app.use(cors());
 
-
+// Mongo DB Connection
+mongoose.connect(mongodb_uri)
+.then(()=>{
+    console.log("Mongo DB is hot and ready");
+})
+.catch((err)=>{
+    console.log(`Failed to connect and ${err}`);
+})
 
 
 
