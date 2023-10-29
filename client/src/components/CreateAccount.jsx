2684f8b577d7a3abf591b22f811131f53430a0eb
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-10-29 22:56:51 +0100

    formik values 1

--- client/src/components/CreateAccount.jsx
+++ client/src/components/CreateAccount.jsx
@@ -4,6 +4,8 @@ import pic from '../components/assets/images/Mobile login.gif';
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import axios from 'axios';
+import { useFormik } from 'formik';
+import * as yup from 'yup'
 
 const CreateAccount = () => {
     let mainDiv = {
@@ -29,6 +31,14 @@ const CreateAccount = () => {
     const auth = getAuth(app)
     const provider = new GoogleAuthProvider()
 
+    let formik = useFormik({
+        initialValues: {
+            email : '',
+            fullname : '',
+            password : ''
+        }
+    })
+
     const signInG = () => {
         // alert("Hello")
         signInWithPopup(auth, provider)
@@ -41,17 +51,18 @@ const CreateAccount = () => {
     }
 
     const signIn = () => {
-        let endpoint = 'http://localhost:5000/user'
-        let payload = {
-            test: "hi"
-        }
-        axios.post(endpoint, payload)
-        .then((result)=>{
-            console.log(result);
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
+        // let endpoint = 'http://localhost:5000/user'
+        // let payload = {
+        //     test: "hi"
+        // }
+        // axios.post(endpoint, payload)
+        // .then((result)=>{
+        //     console.log(result);
+        // })
+        // .catch((err)=>{
+        //     console.log(err);
+        // })
+        console.log(formik);
     }
 
   return (
@@ -71,7 +82,7 @@ const CreateAccount = () => {
                                 <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="1.5" stroke="#141B34" d="M7 8.5L9.94202 10.2394C11.6572 11.2535 12.3428 11.2535 14.058 10.2394L17 8.5"></path>
                                 <path strokeLinejoin="round" strokeWidth="1.5" stroke="#141B34" d="M2.01577 13.4756C2.08114 16.5412 2.11383 18.0739 3.24496 19.2094C4.37608 20.3448 5.95033 20.3843 9.09883 20.4634C11.0393 20.5122 12.9607 20.5122 14.9012 20.4634C18.0497 20.3843 19.6239 20.3448 20.7551 19.2094C21.8862 18.0739 21.9189 16.5412 21.9842 13.4756C22.0053 12.4899 22.0053 11.5101 21.9842 10.5244C21.9189 7.45886 21.8862 5.92609 20.7551 4.79066C19.6239 3.65523 18.0497 3.61568 14.9012 3.53657C12.9607 3.48781 11.0393 3.48781 9.09882 3.53656C5.95033 3.61566 4.37608 3.65521 3.24495 4.79065C2.11382 5.92608 2.08114 7.45885 2.01576 10.5244C1.99474 11.5101 1.99475 12.4899 2.01577 13.4756Z"></path>
                             </svg>
-                            <input placeholder="name@mail.com" title="Inpit title" name="email" type="text" className="input_field" id="email_field"/>
+                            <input placeholder="name@mail.com" title="Inpit title" name="email" onChange={formik.handleChange} type="text" className="input_field" id="email_field"/>
                         </div>
                         <div className="input_container my-4">
                             <label className="input_label" htmlFor="email_field">Full Name</label>
